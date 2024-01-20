3bd7d721896e55a2caeb9f442a23200c514aa24c
Lazaro Ferreira Cabral Neto <lazaroferreiracabral@gmail.com>
2024-01-20 10:00:08 -0300

    Aula Salvando um pedido

--- src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.dto;
 import com.devsuperior.dscommerce.entities.Order;
 import com.devsuperior.dscommerce.entities.OrderItem;
 import com.devsuperior.dscommerce.entities.OrderStatus;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 import java.time.Instant;
@@ -18,6 +19,8 @@ public class OrderDTO {
 
     private ClientDTO client;
     private PaymentDTO payment;
+
+    @NotEmpty(message = "Deve ter pelo menos um item")
     private List<OrderItemDTO> items = new ArrayList<>();
 
     public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
